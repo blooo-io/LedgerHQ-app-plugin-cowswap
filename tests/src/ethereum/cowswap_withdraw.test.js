cd97e540776d61d1ef98060a6cf4d16d7c293ecb
StevenBlooo <steven@blooo.io>
2023-02-24 15:10:48 +0100

    test: added nanos+ test

--- tests/src/ethereum/cowswap_withdraw.test.js
+++ tests/src/ethereum/cowswap_withdraw.test.js
@@ -26,11 +26,11 @@ const devices = [
     label: "Nano X",
     steps: 4, // <= Define the number of steps for this test case and this device
   },
-  // {
-  //   name: "nanosp",
-  //   label: "Nano S+",
-  //   steps: 4, // <= Define the number of steps for this test case and this device
-  // },
+  {
+    name: "nanosp",
+    label: "Nano S+",
+    steps: 4, // <= Define the number of steps for this test case and this device
+  },
 ];
 
 devices.forEach((device) =>
